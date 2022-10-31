380626bd00ebb1df3206b7325147e41d54518930
joonrb <56960550+joonrb@users.noreply.github.com>
2022-10-31 20:38:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,23 +4,29 @@ const thirdAudio = document.getElementById("audio3");
 const fourthAudio = document.getElementById("audio4");
 
 let startButton = document.getElementById("play");
-let background = document.getElementById("background");
+var background = document.getElementById("background");
 
 startButton.addEventListener("click", ()=>{
     firstAudio.play();
+    //background.style.filter = none;
+    //startButton.style.display = none;
+    console.log("HI");
 })
 
 firstAudio.addEventListener("ended", ()=>{
-    background.src = "airport.png"
+    background.style.backgroundImage = "url(2.png)"
     secondAudio.play();
+    console.log("Sec");
 })
 
 secondAudio.addEventListener("ended", ()=>{
-    background.src = "safari.png"
+    background.style.backgroundImage = "url(3.png)"
     thirdAudio.play();
+    console.log("third");
 })
 thirdAudio.addEventListener("ended", ()=>{
-    background.src = "radioStation.png"
-    fourthAudio.play();
+    background.style.backgroundImage = "url(1.png)"
+    //fourthAudio.play();
+    console.log("4");
 })
 
