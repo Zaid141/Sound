119149a64de0705072b772d6958fcd1851155f8b
joonrb <56960550+joonrb@users.noreply.github.com>
2022-11-02 05:10:23 +0900

    Update sound.js

--- sound.js
+++ sound.js
@@ -10,7 +10,7 @@ var background = document.getElementById("background");
 
 startButton.addEventListener("click", ()=>{
     firstAudio.play();
-    background.style.filter = none;
+    background.style.webkitFilter = none;
     startButton.style.display = none;
     console.log("HI");
 })
